8c647b464989a9992812b5ecb767002627a05157
Alexandra Anghel <alexandra@appticles.com>
2018-07-20 13:15:26 +0300

    Move helper methods to separate class

--- google-analytics/python/proc-days-since-last-session.py
+++ google-analytics/python/proc-days-since-last-session.py
@@ -1,35 +1,16 @@
+"""
+Data pre-processing for the days since last session training set
+"""
+
 import pandas as pd
 from pandas.io.json import json_normalize
 import numpy as np
 import re
 import time
+from procbase import ProcessingBase
 
 
-class ProcessingDaysSinceLastSession:
-
-    # Filter clients ids that don't match the GAxxxxxx.xxxxxxx format
-    def eliminate_invalid_client_ids(self, data):
-        pattern = re.compile(r'^GA[0-9]+\.[0-9]+$')
-        filter_data = data['Client ID'].str.contains(pattern)
-
-        return data[filter_data]
-
-    # Transform Device Category values into boolean columns
-    def split_device_category(self, data):
-
-        # split the device category column into multiple columns
-        # multiple by 1 to convert the values to int
-        data['Is Desktop'] = (data['Device Category'] == 'desktop') * 1
-        data['Is Mobile'] = (data['Device Category'] == 'mobile') * 1
-        data['Is Tablet'] = (data['Device Category'] == 'tablet') * 1
-
-        # eliminate the Device Category column
-        return data.drop(['Device Category'], axis=1)
-
-    # Convert a HH:MM:SS time string into seconds
-    def get_sec(self, time_str):
-        h, m, s = time_str.split(':')
-        return int(h) * 3600 + int(m) * 60 + int(s)
+class ProcessingDaysSinceLastSession(ProcessingBase):
 
     # Convert HH:MM:SS columns into seconds
     def convert_time_columns(self, data):
@@ -41,12 +22,6 @@ class ProcessingDaysSinceLastSession:
             self.get_sec)
 
         return data
-
-    def get_number(self, x):
-        if isinstance(x, int):
-            return x
-
-        return int(x.replace(',', ''))
 
     def convert_numeric_columns(self, data):
         data['Entrances'] = data['Entrances'].map(self.get_number)
---
+++ google-analytics/python/procbase.py
@@ -0,0 +1,42 @@
+"""
+Data pre-processing helper functions
+"""
+
+import pandas as pd
+from pandas.io.json import json_normalize
+import numpy as np
+import re
+import time
+
+
+class ProcessingBase:
+
+    # Filter clients ids that don't match the GAxxxxxx.xxxxxxx format
+    def eliminate_invalid_client_ids(self, data):
+        pattern = re.compile(r'^GA[0-9]+\.[0-9]+$')
+        filter_data = data['Client ID'].str.contains(pattern)
+
+        return data[filter_data]
+
+    # Transform Device Category values into boolean columns
+    def split_device_category(self, data):
+
+        # split the device category column into multiple columns
+        # multiple by 1 to convert the values to int
+        data['Is Desktop'] = (data['Device Category'] == 'desktop') * 1
+        data['Is Mobile'] = (data['Device Category'] == 'mobile') * 1
+        data['Is Tablet'] = (data['Device Category'] == 'tablet') * 1
+
+        # eliminate the Device Category column
+        return data.drop(['Device Category'], axis=1)
+
+    # Convert a HH:MM:SS time string into seconds
+    def get_sec(self, time_str):
+        h, m, s = time_str.split(':')
+        return int(h) * 3600 + int(m) * 60 + int(s)
+
+    def get_number(self, x):
+        if isinstance(x, int):
+            return x
+
+        return int(x.replace(',', ''))
