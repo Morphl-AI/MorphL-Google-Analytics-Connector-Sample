7409594c480e0671c56053de48ce64c83d1f59db
Alexandru Constantin <alex@devmotion.ro>
2018-08-10 13:41:24 +0300

    Added static discretize_col and one_hot_encode_col helpers in procbase

--- google-analytics/python/procbase.py
+++ google-analytics/python/procbase.py
@@ -30,6 +30,40 @@ class ProcessingBase:
         # eliminate the Device Category column
         return data.drop(['Device Category'], axis=1)
 
+    # Linear bijective transformation of a column according to the mapping dict
+    # If no mapping dict is passed, then int values starting with 0 will be assigned to distinct values
+    # Normally used to transform textual cols in numeric ones
+    # E.g. ['Yes', 'No', 'No', 'Yes'] can become [1, 0, 0, 1] if mapping = {'Yes': 1, 'No': 0}
+    #
+    # Args:
+    # df = pandas dataframe
+    # col = name of the column to be transformed
+    # mapping (optional) = dict mapping from current value to transformed value
+    #
+    # Return:
+    # df = the dataframe with the "discretized" column
+    # mapping = the mapping used (redundant if you pass in the mapping)
+    @staticmethod
+    def discretize_col(df, col, mapping=None):
+        if mapping is None:
+            unique_vals = df[col].unique()
+            mapping = dict(zip(unique_vals, range(len(unique_vals))))
+        df[col] = df[col].map(mapping)
+        return df, mapping
+
+    # Appends one hot encoding columns to the dataframe for a certain col
+    # Args:
+    # df = pandas dataframe
+    # col = name of the column to be hot encoded
+    #
+    # Return:
+    # df = the dataframe with the hot encoded cols and _without_ the original col
+    @staticmethod
+    def one_hot_encode_col(df, col):
+        one_hot_encoded_cols = pd.get_dummies(df[col], prefix=col, prefix_sep=' ')
+        df_without_original_col = df.drop(col, axis=1)
+        return pd.concat([df_without_original_col, one_hot_encoded_cols], axis=1, sort=False)
+
     # Convert a HH:MM:SS time string into seconds
     def get_sec(self, time_str):
         h, m, s = time_str.split(':')
